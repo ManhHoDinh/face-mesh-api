69a11c63d7c5cfb5a59724f70a5c7396d5b6d117
Hồ Đình Mạnh <21522327@gm.uit.edu.vn>
2023-04-28 21:57:31 +0700

    update api

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI
-
+from typing import Annotated
+from fastapi import FastAPI, File, UploadFile
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World 2"}
+
+@app.post("/files/")
+async def create_file(file: Annotated[bytes, File()]):
+    return {"file_size": len(file)}
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}
